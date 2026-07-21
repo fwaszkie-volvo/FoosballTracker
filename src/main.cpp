03c7516630f8b073032eba8dd38ce0c562267c34
Jakub Cudak <jakub.cudak@volvocars.com>
2026-07-21 10:36:17 +0200

    Rebase

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,5 @@
 #include <opencv2/opencv.hpp>
 
-#include "players-detector.hpp"
-/*
-  Track:
-Possesion - time between a teams first and last touch of a ball, once the ball
-is touched by the opposite team it's in their possesion Shots - amount of balls
-crossing the double defense Shots on target - amount of balls that would've went
-in if not for double defense and goalkeeper
-
-  Algorithm:
-1. Capture video feed (stats don't have to be in real time, a slight delay is
-okay since we are processing 240fps)
-2. Detect ball
-3. Detect if collision with player (if previous frame velocity switched signs
-and collision occured around player)
-
-*/
-
 #include "detector-config.hpp"
 #include "detector.hpp"
 
@@ -28,10 +11,6 @@ int main()
         std::cerr << "Failed to load input image: " << detector_config::kInputImagePath << "\n";
         return 1;
     }
-
-    PlayersDetector players_detector{};
-    players_detector.DetectPlayers(frame);
-    players_detector.DisplayPlayers(frame);
 
     detect_ball(frame);
 
