09cc0328a864fb377b07451263c794497229b50a
Anna Pyszniak <anna.pyszniak@volvocars.com>
2026-07-22 10:54:25 +0200

    Split VideoFrameReader into header and source

--- App/Model/include/private/readers/video_frame_reader.hpp
+++ App/Model/include/private/readers/video_frame_reader.hpp
@@ -6,46 +6,10 @@
 class VideoFrameReader : public IFrameReader
 {
   public:
-    std::optional<cv::Mat> Read() override
-    {
-        if (!capture_.isOpened())
-        {
-            return std::nullopt;
-        }
-
-        cv::Mat frame;
-        capture_ >> frame;
-        if (frame.empty())
-        {
-            return std::nullopt;
-        }
-
-        return frame;
-    }
-
-    bool IsOpened() const override { return capture_.isOpened(); }
-
-    std::optional<double> GetFps() const override
-    {
-        if (!capture_.isOpened())
-        {
-            return std::nullopt;
-        }
-
-        return capture_.get(cv::CAP_PROP_FPS);
-    }
-
-    std::optional<cv::Size> GetFrameSize() const override
-    {
-        if (!capture_.isOpened())
-        {
-            return std::nullopt;
-        }
-
-        const int width = static_cast<int>(capture_.get(cv::CAP_PROP_FRAME_WIDTH));
-        const int height = static_cast<int>(capture_.get(cv::CAP_PROP_FRAME_HEIGHT));
-        return cv::Size(width, height);
-    }
+    std::optional<cv::Mat> Read() override;
+    bool IsOpened() const override;
+    std::optional<double> GetFps() const override;
+    std::optional<cv::Size> GetFrameSize() const override;
 
   protected:
     cv::VideoCapture capture_;
